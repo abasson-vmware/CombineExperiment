06e7397dd1a39ff6c3f1c9b6773dec5123a833eb
alexbasson <alexbasson@gmail.com>
2021-07-23 14:33:08 -0400

    Initial Commit

---
+++ CombineExperiment/CombineExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineExperimentApp.swift
+//  CombineExperiment
+//
+//  Created by Alex Basson on 7/23/21.
+//
+
+import SwiftUI
+
+@main
+struct CombineExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
